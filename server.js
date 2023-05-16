9000f7bde84976cf151b122af28ca65ed8317863
WolfSpiderman <wolfspiderman@yahoo.com>
2023-05-15 20:47:47 -0500

    heroku fix?

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create();
 
 const sess = {
-  secret: process.env.SECRET,
+  secret: 'secret',
   cookie: {
     maxAge: 60 * 60 * 1000,
     httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
   },
   resave: false,
   saveUninitialized: true,
